1507574af82b333fdfcc6d3e962068a96e951412
Leonid Onokhov <sopvop@gmail.com>
2023-06-22 16:12:00 +0300

    Fix possible division by zero in pointInstancer

--- pxr/usdImaging/usdImaging/pointInstancerAdapter.cpp
+++ pxr/usdImaging/usdImaging/pointInstancerAdapter.cpp
@@ -1477,6 +1477,9 @@ UsdImagingPointInstancerAdapter::GetScenePrimPath(
         return SdfPath();
     }
     VtIntArray const & indices = indicesValue.UncheckedGet<VtIntArray>();
+    if (indices.empty() || (instanceIndex % indices.size()) >= indices.size()) {
+        return SdfPath();
+    }
 
     // instanceIndex = parentIndex * indices.size() + i.
     int parentIndex = instanceIndex / indices.size();
